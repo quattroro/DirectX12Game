4160fabd113434a8dbbd303a55a48af05e874dc2
JO <kio7147@naver.com>
2022-07-14 18:18:29 +0900

    1234

---
+++ Client/Game.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "Game.h"
+
+
+
+void Game::Init()
+{
+	HelloEngine();
+}
+
+void Game::Update()
+{
+
+}
---
+++ Client/pch.h
@@ -0,0 +1,7 @@
+#pragma once
+/*자주 활용되는 헤더들을 미리 컴파일 하는 것*/
+
+#pragma comment(lib, "Engine.lib")
+
+#include "EnginePch.h"
+
