68995f9ded7cc729ae48278444bf5c71b5cb77fe
JO <kio7147@naver.com>
2022-07-14 20:14:56 +0900

    1234

---
+++ Engine/DescriptorHeap.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include "DescriptorHeap.h"
+#include "SwapChain.h"
+
+//스왑 체인에서 사용사는 리소스를 설명하는 역할
+//일종의 배열
+void DescriptorHeap::Init(ComPtr<ID3D12Device> device, shared_ptr<SwapChain> swapChain)
+{
+	_swapChain = swapChain;
+
+	// Descriptor (DX12) = View (~DX11)
+	// [서술자 힙]으로 RTV 생성
+	// DX11의 RTV(RenderTargetView), DSV(DepthStencilView), 
+	// CBV(ConstantBufferView), SRV(ShaderResourceView), UAV(UnorderedAccessView)를 전부!
+
+	_rtvHeapSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+	D3D12_DESCRIPTOR_HEAP_DESC rtvDesc;
+	rtvDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	rtvDesc.NumDescriptors = SWAP_CHAIN_BUFFER_COUNT;
+	rtvDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+	rtvDesc.NodeMask = 0;
+
+	// 같은 종류의 데이터끼리 배열로 관리
+	// RTV 목록 : [ ] [ ]
+	device->CreateDescriptorHeap(&rtvDesc, IID_PPV_ARGS(&_rtvHeap));
+
+	D3D12_CPU_DESCRIPTOR_HANDLE rtvHeapBegin = _rtvHeap->GetCPUDescriptorHandleForHeapStart();
+
+	for (int i = 0; i < SWAP_CHAIN_BUFFER_COUNT; i++)
+	{
+		_rtvHandle[i] = CD3DX12_CPU_DESCRIPTOR_HANDLE(rtvHeapBegin, i * _rtvHeapSize);
+		device->CreateRenderTargetView(_swapChain->GetRenderTarget(i).Get(), nullptr, _rtvHandle[i]);
+	}
+}
+
+//스왑체인에서 현재 백버퍼의 인덱스의 RTV를 리턴
+D3D12_CPU_DESCRIPTOR_HANDLE DescriptorHeap::GetBackBufferView()
+{
+	return GetRTV(_swapChain->GetCurrentBackBufferIndex());
+}
