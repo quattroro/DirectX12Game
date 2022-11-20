e0d53d7d12898ff5890c1980c573031f56d490e5
quattroro <kio7147@naver.com>
2022-11-20 19:21:42 +0900

    1234

--- Engine/CommandQueue.h
+++ Engine/CommandQueue.h
@@ -27,13 +27,21 @@ public:
 	ComPtr<ID3D12GraphicsCommandList>	GetGraphicsCmdList() { return _cmdList; }
 	ComPtr<ID3D12GraphicsCommandList>	GetResourceCmdList() { return _resCmdList; }
 private:
+
 	// CommandQueue : DX12에 등장
 	// 외주를 요청할 때, 하나씩 요청하면 비효율적
 	// [외주 목록]에 일감을 차곡차곡 기록했다가 한 방에 요청하는 것
 	ComPtr<ID3D12CommandQueue>			_cmdQueue;
+
+	//CommandQueue에 추가된 명령들은 이 할당자의 메모리에 저장된다.
 	ComPtr<ID3D12CommandAllocator>		_cmdAlloc;
+
+	//CommandQueue의 명령 리스트
+	//한 할당자를 여러 명령 리스트에 연관 시켜도 되지만
+	//명령들을 리스트에 동시에 기록할 수는 없다.
 	ComPtr<ID3D12GraphicsCommandList>	_cmdList;
 
+	//텍스쳐 정보를 넘겨주기 위한 명령 리스트를 따로 만들어 준다.
 	ComPtr<ID3D12CommandAllocator> _resCmdAlloc;
 	ComPtr<ID3D12GraphicsCommandList> _resCmdList;
 
